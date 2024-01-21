f5d5fb46acf88392159e90e17d0439b2250eb9a4
adityapatel-sudo <adityapatel1204@gmail.com>
2024-01-20 20:11:04 -0500

    says my name

--- public/javascripts/dashboardClient.js
+++ public/javascripts/dashboardClient.js
@@ -1,5 +1,7 @@
 //dashboardClient.js
 
+var userObj
+
 console.log(window.location.hash)
 const accessToken = window.location.hash.split('#')[1].split('&')[0].split("access_token=")[1]
 console.log(accessToken)
@@ -8,14 +10,20 @@ const userIDUrl = "https://api.spotify.com/v1/me"
 const headers = new Headers({
     'Authorization': "Authorization: Bearer  "+accessToken,
 });
+
 fetch(userIDUrl,{
     method: 'GET',
     headers: headers
 })
-    .then(response => console.log(response.json()))
-    .then(data => {
-        console.log('Response from server:', data);
+    .then(result => {
+        userObj = result.json().then(userData => {
+            console.log(userData)
+            console.log(userData.display_name)
+            const displayName = userData.display_name;
+            const textWelcome = document.getElementById("text_welcome")
+            textWelcome.innerText = "Welcome " + displayName
+        })
     })
     .catch(error => {
         console.error('Error:', error);
-    });
+    })
