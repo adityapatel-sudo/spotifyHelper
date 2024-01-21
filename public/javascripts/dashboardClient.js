c043885617c76eeadc4a5c9a4dd4b8d76833c3d9
adityapatel-sudo <adityapatel1204@gmail.com>
2024-01-20 20:53:47 -0500

    getting playslists URL

--- public/javascripts/dashboardClient.js
+++ public/javascripts/dashboardClient.js
@@ -2,12 +2,16 @@
 
 var userObj
 var userData
+var userID
+var simplifiedPlaylists
 
 console.log(window.location.hash)
 const accessToken = window.location.hash.split('#')[1].split('&')[0].split("access_token=")[1]
 console.log(accessToken)
 
 const userIDUrl = "https://api.spotify.com/v1/me"
+const playlistsUrl = "https://api.spotify.com/v1/users/"
+const playlistsUrl2 = "/playlists"
 const headers = new Headers({
     'Authorization': "Authorization: Bearer  "+accessToken,
 });
@@ -21,10 +25,22 @@ fetch(userIDUrl,{
             console.log(data)
             console.log(data.display_name)
             const displayName = data.display_name;
+            userID = data.id
             const textWelcome = document.getElementById("text_welcome")
             textWelcome.innerText = "Welcome " + displayName
+
+            var playListsUrlFinal = playlistsUrl + userID + playlistsUrl2
+            fetch(playListsUrlFinal, {
+                method: 'GET',
+                headers: headers
+            }).then(result => {
+                result.json().then(data => {
+                    console.log(data)
+                })
+            })
         })
     }).catch(error => {
         console.error('Error:', error);
     })
 
+const playlistsIDUrl = "https://api.spotify.com/v1/me"
